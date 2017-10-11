78284ed2210e4ceaa9fc7a5cd3be71a40cc92ac3
Artur <artur.shabunov@gmail.com>
2017-10-11 09:46:31 +0300

    New php files (class and index)

---
+++ course.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Shabunov Ulesanne 2</title>
+</head>
+<body>
+<?php
+class Course{
+    private $namecourse;
+    private $
+}
+
+
+?>
+
+</body>
+</html>
