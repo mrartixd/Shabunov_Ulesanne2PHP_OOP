989eb7522b5cbd021591f1c10eb6ea7c8db38729
Artur Shabunov <artixd@yandex.ru>
2017-10-17 17:54:35 +0300

    task list, hide warning

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
 При оценивании также будет учитываться внешний вид пользовательского интерфейса
 
 Используйте git при пректировании проекта -->
-<?php include course.php
-include students.php ?>
+<?php error_reporting( E_ERROR ); ?>
+<?php 
+include course.php;
+include students.php; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,6 +21,18 @@ include students.php ?>
 <title>Shabunov Ulesanne 2</title>
 </head>
 <body>
+<div class="row">
+	<div class="col-12 col-md-4">
+		<ul class="list-group">
+			<li class="list-group-item active">Задание:</li>
+			<li class="list-group-item">Создайте Web приложение, которое содержит информацию о курсах и студентах, которые записались на курсы</li>
+			<li class="list-group-item">На главной странице выводятся название курсов и их код</li>
+			<li class="list-group-item">Пользователь может выбрать курс и посмотреть на другой странице полную информацию о курсе и кто записался на данный курс</li>
+			<li class="list-group-item">Для реализации создайте классы, которые описывают модель системы</li>
+			<li class="list-group-item">При оценивании также будет учитываться внешний вид пользовательского интерфейса</li>
+            <li class="list-group-item">Используйте git при пректировании проекта</li>
+		</ul>
+	</div>
 <script href="js/bootstrap.js" type="text/javascript"></script>
 </body>
 </html>
