4b1172e6e39ef02773959789b82951b079215c86
Artur Shabunov <artixd@yandex.ru>
2017-10-18 00:35:12 +0300

    infocourse

--- infocourse.php
+++ infocourse.php
@@ -1,10 +1,16 @@
+<?php error_reporting( E_ERROR ); ?>
+<?php 
+include course.php;
+include students.php; ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
+<link href="css/bootstrap.css" rel="stylesheet">
 <title>Shabunov Ulesanne 2</title>
 </head>
 <body>
-
+<script href="js/bootstrap.js" type="text/javascript"></script>
 </body>
+<footer style="padding-top: 80px;"><div class="fixed-bottom p-3 bg-info text-white">Artur Shabunov 2017</div></footer>
 </html>
