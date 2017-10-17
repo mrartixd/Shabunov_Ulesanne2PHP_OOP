3f5525f905535b2de853cc718ca4b8eb6f7abd02
Artur Shabunov <artixd@yandex.ru>
2017-10-17 18:42:17 +0300

    fixes

--- index.php
+++ index.php
@@ -21,18 +21,23 @@ include students.php; ?>
 <title>Shabunov Ulesanne 2</title>
 </head>
 <body>
-<div class="row">
-	<div class="col-12 col-md-4">
+<div class="container" style="padding-top: 30px">
+    <div class="row">
+	    <div class="col-12 col-md-4">
 		<ul class="list-group">
-			<li class="list-group-item active">Задание:</li>
+			<li class="list-group-item list-group-item-info">Задание:</li>
 			<li class="list-group-item">Создайте Web приложение, которое содержит информацию о курсах и студентах, которые записались на курсы</li>
 			<li class="list-group-item">На главной странице выводятся название курсов и их код</li>
 			<li class="list-group-item">Пользователь может выбрать курс и посмотреть на другой странице полную информацию о курсе и кто записался на данный курс</li>
 			<li class="list-group-item">Для реализации создайте классы, которые описывают модель системы</li>
 			<li class="list-group-item">При оценивании также будет учитываться внешний вид пользовательского интерфейса</li>
-            <li class="list-group-item">Используйте git при пректировании проекта</li>
+            <li class="list-group-item list-group-item-success">Используйте git при пректировании проекта</li>
 		</ul>
 	</div>
+</div>
+
+
 <script href="js/bootstrap.js" type="text/javascript"></script>
 </body>
+<footer style="padding-top: 80px;"><div class="fixed-bottom p-3 bg-info text-white">Artur Shabunov 2017</div></footer>
 </html>
